962778004b0577c8b6a6c85d2ee29d8b1d390b74
Nysonn <nysonagumya@gmail.com>
2024-12-08 00:25:40 +0300

    new

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ const app = express();
 
 // PostgreSQL Database connection
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  user: process.env.PG_USER || "postgres",
+  host: process.env.PG_HOST || "localhost",
+  database: process.env.PG_DATABASE || "e_doctor_app",
+  password: process.env.PG_PASSWORD || "producer2022", // Ensure this is a valid string
+  port: parseInt(process.env.PG_PORT) || 5432,
 });
 
 db.connect();
